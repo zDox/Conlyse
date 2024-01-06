07bd77d1073c7c1c01ddc24af9aa7b7537399fa3
zDox <zdox@artixus.de>
2024-01-06 19:02:17 +0100

    Better GameInfo parser

--- data_types.py
+++ data_types.py
@@ -4,7 +4,38 @@ from enum import Enum
 
 
 class GameInfoState(Enum):
+    UNDEFINED = -2
+    NONE = -1
     READY_TO_JOIN = 0
+    RUNNING = 1
+
+    @classmethod
+    def from_string(cls, string):
+        match string:
+            case None:
+                return cls.UNDEFINED
+            case "readytojoin":
+                return cls.READY_TO_JOIN
+            case "running":
+                return cls.RUNNING
+
+
+def timestamp_to_datetime(obj, timestamp):
+    return datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S") \
+            if timestamp else None
+
+
+def unixtimestamp_to_datetime(obj, timestamp):
+    return datetime.utcfromtimestamp(int(timestamp)) \
+            if timestamp else None
+
+
+def openslots_to_currentplayers(obj, openslots):
+    return int(obj["nrofplayers"]) - int(openslots)
+
+
+def timescale_to_speedfactor(obj, time_scale):
+    return int(1/float(time_scale))
 
 
 @dataclass
@@ -68,12 +99,12 @@ class GameInfo():
             'scenario_id': 'scenarioID',
             'scenario_version_id': 'scenarioVersionID',
             'season_id': 'seasonID',
-            'start_of_game': 'startofgame2',
-            'current_players': 'openSlots',
+            'start_of_game': ['startofgame2', unixtimestamp_to_datetime],
+            'current_players': ['openSlots', openslots_to_currentplayers],
             'max_players': 'nrofplayers',
             'day_of_game': 'dayofgame',
             'end_of_game': 'endofgame2',
-            'last_login': 'lastlogintime',
+            'last_login': ['lastlogintime', unixtimestamp_to_datetime],
             'creator_id': 'creatorID',
             'title': 'title',
             'comment': 'comment',
@@ -82,7 +113,7 @@ class GameInfo():
             'demo_game': 'demoGame',
             'min_rank': 'minRank',
             'managed_game': 'managedGame',
-            'creation_date': 'crdate',
+            'creation_date': ['crdate', unixtimestamp_to_datetime],
             'ai_level': 'aiLevel',
             'victory_points': 'victoryPoints',
             'victory_condition': 'victoryCondition',
@@ -102,50 +133,38 @@ class GameInfo():
             'deleted': 'deleted',
             'gold_round': 'goldRound',
             'game_status': 'gameStatus',
-            'last_statistics_update': 'lastStatisticsUpdate',
+            'last_statistics_update': ['lastStatisticsUpdate',
+                                       unixtimestamp_to_datetime],
             'unit_pack': 'unitPack',
             'xp_boost_factor': 'xpBoostFactor',
-            'speed_factor': 'timeScale',
-            'update_timestamp': 'updateTstamp',
+            'speed_factor': ['timeScale', timescale_to_speedfactor],
+            'update_timestamp': ['updateTstamp', timestamp_to_datetime],
             'max_rank': 'maxRank',
             'max_join_day': 'maxJoinDay',
             'map_reference': 'mapReference',
             'state': 'state',
         }
-        field_names = [f.name for f in cls.__dataclass_fields__.values()]
+        field_names = cls.__annotations__.keys()
         parsed_data = {}
         for f_name in field_names:
-            val = obj[json_field_mapping[f_name]]
+            if isinstance(json_field_mapping[f_name], list):
+                parsed_data[f_name] = json_field_mapping[f_name][1](
+                        obj,
+                        obj[json_field_mapping[f_name][0]])
+                continue
+            else:
+                val = obj[json_field_mapping[f_name]]
+
             if not val:
                 parsed_data[f_name] = None
                 continue
-            if val.isdigit():
+
+            if cls.__annotations__[f_name] == int:
                 parsed_data[f_name] = int(val)
+            elif cls.__annotations__[f_name] == bool:
+                parsed_data[f_name] = bool(int(val))
             else:
                 parsed_data[f_name] = val
 
-        """
-        parsed_data = {
-            k: int(obj[json_field_mapping[k]])
-            if  else
-            obj[json_field_mapping[k]]
-            for k in field_names
-        }
-        """
-        # Custom conversions for specific fields
-        parsed_data['start_of_game'] = datetime.utcfromtimestamp(
-                int(obj.get('startofgame2', 0)))
-        parsed_data['last_login'] = datetime.utcfromtimestamp(
-                int(obj.get('lastlogintime', 0))
-                ) if obj.get('lastlogintime') else None
-        parsed_data['creation_date'] = datetime.utcfromtimestamp(
-                int(obj.get('crdate', 0)))
-        parsed_data['last_statistics_update'] = datetime.utcfromtimestamp(
-                int(obj.get('lastStatisticsUpdate', 0)))
-        print(obj.get('updateTstamp'))
-        parsed_data['update_timestamp'] = datetime.strptime(
-                obj.get('updateTstamp'), "%Y-%m-%d %H:%M:%S")
-
-        parsed_data['state'] = GameInfoState.READY_TO_JOIN \
-            if obj.get("state") == "ready" else None
+        parsed_data['state'] = GameInfoState.from_string(obj.get("state"))
         return cls(**parsed_data)
--- example.py
+++ example.py
@@ -1,10 +1,11 @@
 from web_api import WebAPI
 import creds
-from json import dumps
+from pprint import pprint
+
 
 if __name__ == "__main__":
     web_api = WebAPI()
     web_api.login(creds.username, creds.password)
-    print(web_api.auth)
+    pprint(web_api.auth)
     res = web_api.get_international_games()
-    print(res)
+    pprint(res)
