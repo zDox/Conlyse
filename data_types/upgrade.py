903a071c18d3e56b0804ae153e6e61901cd853d9
zDox <zdox@artixus.de>
2024-01-28 13:45:04 +0100

    Added UnitFeature and UpgradeFeature

--- data_types/upgrade.py
+++ data_types/upgrade.py
@@ -2,12 +2,62 @@ from data_types.utils import JsonMappedClass, MappedValue
 
 from dataclasses import dataclass
 from datetime import timedelta
+from enum import Enum
 
 
 def parse_dict(obj):
     obj.pop("@c")
     return {int(key): value
             for key, value in obj.items()}
+
+
+class UpgradeFeature(Enum):
+    FORTRESS = 0,
+    HARBOUR = 1,
+    RAILROAD = 2,
+    FACTORY = 3,
+    CAPITAL = 4,
+    MORALE_BONUS = 5,
+    RECRUIT = 6,
+    FACTORY_TECH = 7,
+    AIRFIELD = 8,
+    INDESTRUCTIBLE = 9,
+    PUBLIC = 10,
+    CONSTRUCTION_SLOTS = 12,
+    PRODUCTION_SLOTS = 13,
+    INVISIBLE = 14,
+    NO_SPEED_UP = 15,
+    INDESTRUCTABLE = 16,
+    STATUS_CHANGE = 17,
+    REPORT_PROGRESS = 18,
+    VICTORY_POINTS = 19,
+    SIEGE_DELAY = 20,
+    MINIMUM_SIEGE = 21,
+    RESEARCH_LIMIT_LEVEL = 23,
+    MAX_MANPOWER = 24,
+    STACKABLE = 25,
+    SHOW_IN_NEWSPAPER = 26,
+    BUILDING_PLOTS = 27,
+    USED_BUILDING_PLOTS = 28,
+    CAN_SPEEDUP_CONSTRUCTION = 31,
+    CONSTRUCTION_CLASS = 32,
+    CAN_HEAL_ARMIES = 34,
+    DEMOLISHABLE = 38,
+    NOT_ADDED_TO_PROVINCE = 39,
+    INVISIBLE_FROM_STATS = 40
+
+
+def parse_features(obj):
+    obj.pop("@c")
+    return {UpgradeFeature(key): value
+            for key, value in obj.items()}
+
+
+class ValueFunction(Enum):
+    VALUE_FUNCTION_LINEAR = 0,
+    VALUE_FUNCTION_SQRT = 1,
+    VALUE_FUNCTION_PANZERWARS = 2,
+    VALUE_FUNCTION_STEP = 3
 
 
 @dataclass
@@ -25,7 +75,7 @@ class UpgradeType(JsonMappedClass):
     daily_costs: dict[int, int]
     daily_productions: dict[int, int]
     production_bonus: dict[int, float]
-    features: dict[int, float]
+    features: dict[UpgradeFeature, float]
     # feature_functions -> Dont know how to implement
     # build_time_functions -> Dont know how to implement
     replaced_upgrade: int
@@ -52,7 +102,7 @@ class UpgradeType(JsonMappedClass):
         "daily_costs": MappedValue("dc", parse_dict),
         "daily_productions": MappedValue("dp", parse_dict),
         "production_bonus": MappedValue("pb", parse_dict),
-        "features": MappedValue("f", parse_dict),
+        "features": MappedValue("f", parse_features),
         "replaced_upgrade": "ru",
         "required_upgrades": MappedValue("rqu", parse_dict),
         "feature_icon_prefix": "fip",
