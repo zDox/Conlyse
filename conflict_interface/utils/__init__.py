6e7f41e6c98a93c7b28e42df7310468d4cbb716a
zDox <zdox@artixus.de>
2024-02-06 21:48:46 +0100

    Added Rect class

--- conflict_interface/utils/__init__.py
+++ conflict_interface/utils/__init__.py
@@ -1,5 +1,6 @@
 from .json_mapped_class import JsonMappedClass, MappedValue, UpdatableClass, \
         DefaultEnumMeta
 from .point import Point
+from .rect import Rect
 from .helper import timestamp_to_datetime, unixtimestamp_to_datetime, \
         seconds_to_timedelta, milliseconds_to_timedelta
