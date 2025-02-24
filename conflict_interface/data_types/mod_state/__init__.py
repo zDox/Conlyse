d1868fa0ca40698e3a555e8563eb6e73a3afe5bd
jannik <zdox@artixus.de>
2025-02-24 11:53:59 +0100

    Moved army and unit to army_state.py

--- conflict_interface/data_types/mod_state/__init__.py
+++ conflict_interface/data_types/mod_state/__init__.py
@@ -2,8 +2,7 @@ from .mod_state import ModState
 from .air_parameters import AirParameters
 from .anti_air_parameters import AntiAirParameters
 from .commands import Command
-from .modable_unit import Unit, SpecialUnit
-from .unit import Unit
 from .unit_feature import UnitFeature
 from .unit_type import UnitType
 from .upgrade import UpgradeType, UpgradeFeature, ModableUpgrade
+from .modable_unit import SpecialUnit
