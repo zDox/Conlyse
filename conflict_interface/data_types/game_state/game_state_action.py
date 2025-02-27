0849ddb688343a773cb8828555341e387553de32
NikNam3 <96829112+NikNam3@users.noreply.github.com>
2025-02-27 12:55:31 +0100

    Delete conflict_interface/data_types/game_state/game_state_action.py

--- conflict_interface/data_types/game_state/game_state_action.py
+++
@@ -1,9 +0,0 @@
-"""
-"@c": "ultshared.action.UltUpdateGameStateAction",
-"stateType": 0,
-"stateID": "0",
-"addStateIDsOnSent": True,
-"option": None,
-"stateIDs": self.state_ids,
-"tstamps": self.time_stamps,
-"""
