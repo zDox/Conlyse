fe255ecbaf30fe10369720e9bfed0b477ba4c448
zdox <zdox@artixus.de>
2025-03-09 11:32:17 +0100

    Fixed Registration.

--- examples/list_upgrades.py
+++ examples/list_upgrades.py
@@ -1,20 +1,23 @@
 import logging
 
-from conflict_interface import HubInterface
 
 from pprint import pprint
 
+from conflict_interface.hub_interface import HubInterface
 from conflict_interface.logger_config import setup_library_logger
 from examples.helper_functions import load_credentials
 
 if __name__ == "__main__":
     setup_library_logger(logging.DEBUG)
     interface = HubInterface()
-    username, password, email, proxy_url = load_credentials()
-    interface.login(username, password)
-    print("Starting example")
-
-    pprint(f"Joining new game:  {9709963}")
-    game = interface.join_game(9709963)
-
-    pprint(game.get_upgrade_types(upgrade_identifier="Arms Industry"))
+    interface.set_proxy({
+        "http": "http://127.0.0.1:8080",
+        "https": "http://127.0.0.1:8080"
+    })
+    interface.api.set_certificate("burp_suite_certificate.pem")
+    print(interface.register("fgdljehjggfojkgtesa", "tesdstdiffdjkhgfhf@gmail.com", "gjkhjhgjhdugoh"))
+    """
+    res= interface.register(username="eQVnARsGDSfdsfgfgwyfdfaC",
+                       email="WODnGzzZpncXfdssgfgdfsdffFn@gmail.com",
+                       password="dshfjshisafhjiios")
+    """
