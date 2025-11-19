52bc53cbff19e4506d036b900b0010895b0a708c
zdox <zdox@artixus.de>
2025-11-19 18:45:18 +0100

    Refactor read_replay.py and replay_database.py: remove unused imports and simplify connection handling

--- examples/read_replay.py
+++ examples/read_replay.py
@@ -1,19 +1,11 @@
 import logging
-import pickle
-from dataclasses import dataclass
-from pprint import pprint
 from time import time
-
-import zstandard as zstd
 
 from conflict_interface.interface.game_interface import GameInterface
 from conflict_interface.interface.replay_interface import ReplayInterface
 from conflict_interface.logger_config import setup_library_logger
+from conflict_interface.utils.helper import datetime_to_unix_ms
 
-
-@dataclass
-class B:
-    foo: int
 if __name__ == "__main__":
     setup_library_logger(logging.DEBUG)
     logging.basicConfig(level=logging.DEBUG)
