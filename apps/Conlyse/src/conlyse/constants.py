ebe7a00929271de9694b5f12a9c3e80b4f2fc654
zdox <zdox@artixus.de>
2025-12-16 22:02:45 +0100

    feat: change default start replay page to MapPage

--- apps/Conlyse/src/conlyse/constants.py
+++ apps/Conlyse/src/conlyse/constants.py
@@ -3,4 +3,4 @@ from conlyse.utils.enums import PageType
 APPLICATION_NAME = "Conlyse"
 
 START_PAGE = PageType.ReplayListPage
-START_REPLAY_PAGE = PageType.PlayerListPage
+START_REPLAY_PAGE = PageType.MapPage
