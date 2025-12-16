a30ab148fd897f408f9dabcb88aca0630195da54
zdox <zdox@artixus.de>
2025-12-16 20:15:42 +0100

    Refactor replay hook system to use ReplayHookTag for event identification

--- conflict_interface/hook_system/replay_hook_event.py
+++ conflict_interface/hook_system/replay_hook_event.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
 from typing import Any
 
+from conflict_interface.hook_system.replay_hook_tag import ReplayHookTag
+
 
 @dataclass
 class ReplayHookEvent:
     reference: Any
     attributes: dict[str, Any]
-    tag: str
+    tag: ReplayHookTag
