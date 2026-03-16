d1e471ce6b8938da376ccbada6a52ca3ac9c5037
zdox <zdox@artixus.de>
2026-03-16 22:07:20 +0100

    Update documentation URLs and remove Conlyse iml file
    - Updated the Docusaurus configuration to reflect the new documentation URL and base path.
    - Removed the `.idea/Conlyse.iml` file

--- apps/docs/docusaurus.config.ts
+++ apps/docs/docusaurus.config.ts
@@ -16,10 +16,10 @@ const config: Config = {
 
   // Set the production url of your site here
   // Adjust this to match where you host the docs (e.g. GitHub Pages).
-  url: 'https://zdox.github.io',
+  url: 'https://docs.conlyse.zdox.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Conlyse/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
